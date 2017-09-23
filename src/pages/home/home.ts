481297c5d94d432c02690d191d7f3d18266b8297
seedgabo <seedgabo@gmail.com>
2017-09-23 07:39:16 -0500

    remove unuserd pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,6 @@
-import { EventsPage } from './../events/events';
-import { ProfilePage } from './../profile/profile';
-import { InvoicesPage } from './../invoices/invoices';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Api } from "../../providers/api";
-import { PostsPage } from "../posts/posts";
 // import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
@@ -24,57 +20,9 @@ export class HomePage {
 
   ionViewDidLoad() {
     this.api.startEcho();
-    this.getSliders();
-    this.getNextEvents()
-    this.getNews();
   }
-
-  getSliders() {
-    this.api.storage.get('sliders').then((sliders) => {
-      if (sliders)
-        this.sliders = sliders;
-      this.api.get("sliders?with[]=image")
-        .then((data: any) => {
-          this.api.storage.set('sliders', data);
-          console.log(data);
-          this.sliders = data;
-        })
-        .catch((error) => {
-          console.error("error trayendo los sliders:", error);
-        });
-    });
-
+  panic() {
+    this.api.panic();
   }
-  getNextEvents() {
-    this.api.get('events?scope[soon]')
-      .then((data: any) => {
-        this.nextEvents = data;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-  getNews() {
-    this.api.get('posts?order[created_at]=desc&limit=7&with[]=user.residence&with[]=image')
-      .then((data: any) => {
-        console.log("news", data);
-        this.news = data;
-      })
-      .catch((err) => {
-      });
-  }
-  gotoNews() {
-    this.navCtrl.push(PostsPage);
-  }
-  gotoInvoices() {
-    this.navCtrl.push(InvoicesPage)
-  }
-  gotoProfile() {
-    this.navCtrl.push(ProfilePage);
-  }
-  gotoCalendar(event) {
-    this.navCtrl.push(EventsPage, { event: event });
-  }
-
 }
 
